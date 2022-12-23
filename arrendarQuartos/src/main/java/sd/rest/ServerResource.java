1c370c2656d1049149f0a5e8133e5af9de835d62
guigrilo <bacanogui@gmail.com>
2022-12-23 01:15:14 +0000

    Done (só falta o publish/subscribe e a segurança (acho))

--- arrendarQuartos/src/main/java/sd/rest/ServerResource.java
+++ arrendarQuartos/src/main/java/sd/rest/ServerResource.java
@@ -2,9 +2,7 @@ package sd.rest;
 
 import jakarta.ws.rs.*;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.*;
+import java.util.List;
 
 /**
  * Root resource (exposed at "server" path)
@@ -27,14 +25,48 @@ public class ServerResource {
     @Path("/ads")
     @GET
     @Produces({"application/json"})
-    public synchronized List<Ad> getAds(@QueryParam("state") String state, @QueryParam("aid") String aid, @QueryParam("description") String description, @QueryParam("local") String local) {
-        String fields = state;
-        if(!aid.equals("") || aid != null)
-            fields += aid;
-        if(!description.equals("") || description != null)
-            fields += description;
-        if(!local.equals("") || local != null)
-            fields += local;
-        return q.consultTableAdvertisement(fields);
+    public synchronized List<Ad> searchAds(@QueryParam("state") String state, @QueryParam("description") String description, @QueryParam("local") String local, @QueryParam("aid") String aid) {
+        String fields = "statead=" + state;
+        if(state == null) {
+            fields = "aid=" + aid;
+            return q.consultTableAdvertisement(fields);
+        }
+        if(description == null && local == null && aid == null)
+            return q.consultTableAdvertisement(fields);
+        else if(aid == null) {
+            if(local == null || local.trim().length() == 0) {
+                fields += "&description=" + description;
+                return q.consultTableAdvertisement(fields);
+            }
+            else {
+                fields += "&description=" + description + "&localad=" + local;
+                return q.consultTableAdvertisement(fields);
+            }
+        }
+        else {
+            fields += "&aid=" + aid;
+            return q.consultTableAdvertisement(fields);
+        }
+    }
+
+    @Path("/msg")
+    @POST
+    @Consumes({"application/json"})
+    public synchronized boolean sendMessage(Message msg) {
+        return q.insertIntoTableMessages(msg);
+    }
+
+    @Path("/msgs")
+    @GET
+    @Produces({"application/json"})
+    public synchronized List<Message> getMessages(@QueryParam("aid") String aid) {
+        return q.consultTableMessages(aid);
+    }
+
+    @Path("/changead")
+    @POST
+    @Consumes({"application/json"})
+    public synchronized boolean changeAdState(@QueryParam("state") String state, @QueryParam("aid") String aid) {
+        return q.alterTableAdvertisement(state, aid);
     }
 }
